cfd1d187f7f259d34807b1affd1235abcac66098
Phan Tan Dung <phantandung92@gmail.com>
2016-05-18 14:45:40 +0700

    Gói nâng cấp lên Laws 4.0 Official

--- themes/default/js/laws.js
+++
@@ -1,6 +0,0 @@
-/**
- * @Project NUKEVIET 3.0
- * @Author VINADES.,JSC (contact@vinades.vn)
- * @Copyright (C) 2010 VINADES.,JSC. All rights reserved
- * @Createdate Wed, 27 Jul 2011 14:55:22 GMT
- */
