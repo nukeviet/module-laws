4585cb1160f7be24eebaeca382340fcf8c4920d2
VINADES.,JSC <contact@vinades.vn>
2021-12-23 11:34:23 +0700

    Sửa lỗi phân trang ở trang chủ

--- modules/laws/funcs/main.php
+++ modules/laws/funcs/main.php
@@ -17,23 +17,12 @@ $key_words = $module_info['keywords'];
 $page_url = $base_url = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name;
 
 $page = 1;
-$issetPage = false;
-if (isset($array_op[0])) {
-    if (preg_match('/^page\-([0-9]{1,10})$/', $array_op[0], $m)) {
-        $page = intval($m[1]);
-        $issetPage = true;
-    } else {
-        nv_redirect_location($base_url);
-    }
-}
-
-if ($page < 1 or ($issetPage and $page < 2)) {
-    nv_redirect_location($base_url);
+if (isset($array_op[0]) and preg_match('/^page\-([0-9]{1,10})$/', $array_op[0], $m)) {
+    $page = intval($m[1]);
 }
 
 $contents = $cache_file = '';
 $per_page = $nv_laws_setting['nummain'];
-
 
 if (!defined('NV_IS_MODADMIN') and $page < 5) {
     $cache_file = NV_LANG_DATA . '_' . $module_info['template'] . '_' . $op . '_' . $page . '_' . NV_CACHE_PREFIX . '.cache';
@@ -44,7 +33,7 @@ if (!defined('NV_IS_MODADMIN') and $page < 5) {
 
 if (empty($contents)) {
     if ($nv_laws_setting['typeview'] != 2) {
-        // Danh sách văn bản dạng list. typeview=2 => Phân theo cơ quan ban hành
+        // Danh sách văn bản dạng list, có phân trang
         $order = ($nv_laws_setting['typeview'] == 1 or $nv_laws_setting['typeview'] == 4) ? "ASC" : "DESC";
         $order_param = ($nv_laws_setting['typeview'] == 0 or $nv_laws_setting['typeview'] == 1) ? "publtime" : "addtime";
 
@@ -55,24 +44,25 @@ if (empty($contents)) {
         $query = $db->query("SELECT FOUND_ROWS()");
         $all_page = $query->fetchColumn();
 
-        betweenURLs($page, ceil($all_page/$per_page), $base_url, '/page-', $prevPage, $nextPage);
+        betweenURLs($page, ceil($all_page / $per_page), $base_url, '/page-', $prevPage, $nextPage);
 
         $generate_page = nv_alias_page($page_title, $base_url, $all_page, $per_page, $page);
         $array_data = raw_law_list_by_result($result, $page, $per_page);
 
         if ($page > 1) {
-            if(empty($array_data)){
+            if (empty($array_data)) {
                 nv_redirect_location($base_url);
-            }else{
-                $page_url .= '&amp;' . NV_OP_VARIABLE . '/page-' . $page;
+            } else {
+                $page_url .= '&amp;' . NV_OP_VARIABLE . '=page-' . $page;
             }
         }
-        
+
         $canonicalUrl = getCanonicalUrl($page_url);
 
         $contents = nv_theme_laws_main($array_data, $generate_page);
     } else {
-        // Văn bản phân theo cơ quan ban hành
+        // Văn bản phân theo cơ quan ban hành, không phân trang
+        $canonicalUrl = getCanonicalUrl($page_url);
         if (!empty($nv_laws_listsubject)) {
             foreach ($nv_laws_listsubject as $subjectid => $subject) {
                 $result = $db->query('SELECT * FROM ' . NV_PREFIXLANG . '_' . $module_data . '_row WHERE sid=' . $subjectid . ' AND status=1 ORDER BY addtime DESC LIMIT ' . $subject['numlink']);
