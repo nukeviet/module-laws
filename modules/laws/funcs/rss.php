0b61a94e0dc776401585498f0f325e4a7731d782
Nguyễn Nam <ct17.nguyenluongnam@gmail.com>
2022-04-25 08:45:36 +0700

    Fix code chuẩn PSR-12

--- modules/laws/funcs/rss.php
+++ modules/laws/funcs/rss.php
@@ -38,25 +38,25 @@ if (!empty($nv_laws_listcat)) {
         $channel['link'] = NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;cat=" . $nv_laws_listcat[$cid]['alias'];
         $channel['description'] = $nv_laws_listcat[$cid]['introduction'];
 
-        $sql = "SELECT id, title, alias, introtext, addtime
+        $sql = "SELECT id, title, code, alias, introtext, publtime
         FROM " . NV_PREFIXLANG . "_" . $module_data . "_row WHERE cid=" . $cid . "
         AND status=1 ORDER BY edittime DESC LIMIT 30";
     } else {
         $in = array_keys($nv_laws_listcat);
         $in = implode(",", $in);
-        $sql = "SELECT id, title, code, alias, introtext, addtime
+        $sql = "SELECT id, title, code, alias, introtext, publtime
         FROM " . NV_PREFIXLANG . "_" . $module_data . "_row WHERE cid IN (" . $in . ")
         AND status=1 ORDER BY edittime DESC LIMIT 30";
     }
     if ($module_info['rss']) {
         if (($result = $db->query($sql)) !== false) {
-            while (list ($id, $title, $code, $alias, $introtext, $addtime) = $result->fetch(3)) {
+            while (list ($id, $title, $code, $alias, $introtext, $publtime) = $result->fetch(3)) {
                 $items[] = [
-                    'title' => '['. $code .']' . $title,
+                    'title' => '[' . $code . ']' . $title,
                     'link' => NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=detail/" . $alias,
                     'guid' => $module_name . '_' . $id,
                     'description' => $introtext,
-                    'pubdate' => $addtime
+                    'pubdate' => $publtime
                 ];
             }
         }
