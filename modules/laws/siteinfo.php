6be24428d135931275472e4e4f5a92bc377a2c96
Phan Tấn Dũng <phantandung92@gmail.com>
2023-08-06 16:15:57 +0700

    Cập nhật ngôn ngữ trong siteinfo.php

--- modules/laws/siteinfo.php
+++ modules/laws/siteinfo.php
@@ -13,13 +13,11 @@ if (!defined('NV_IS_FILE_SITEINFO')) {
     die('Stop!!!');
 }
 
-$lang_siteinfo = nv_get_lang_module($mod);
-
 // Tong so bai viet
 $number = $db->query("SELECT COUNT(*) as number FROM " . NV_PREFIXLANG . "_" . $mod_data . "_row")->fetchColumn();
 if ($number > 0) {
     $siteinfo[] = [
-        'key' => $lang_siteinfo['siteinfo_numlaws'],
+        'key' => $nv_Lang->getModule('siteinfo_numlaws'),
         'value' => $number
     ];
 }
